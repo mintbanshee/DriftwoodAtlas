3a6d7ee76d6eb340d9a2fc795a79f21ed0b624ad
mintbanshee <alexandria.mcqueen@triosstudent.com>
2025-11-08 22:30:37 -0500

    Assets, Shell & Home

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("sprout web scaffold ready");
+
+let menuOpen = false;
+function toggleMenu() { 
+  if(!menuOpen) { // open menu
+    $('nav').animate({
+      right: 0
+    }, 420, 'swing');
+  }
+  else { // close menu
+    $('nav').animate({
+      right: -350
+    }, 420, 'swing');
+  }
+  // toggles the state
+  menuOpen = !menuOpen; 
+} // toggleMenu()
